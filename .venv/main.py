27f1b29eb09750d54d6b8489e42c4259af1e98ee
LiamBenZaken <liambz3210@gmail.com>
2023-12-17 16:04:59 +0200

    finished the functions

--- .venv/main.py
+++ .venv/main.py
@@ -1,2 +1,25 @@
 #starting program the calculate
+def add(nums):
+    print(nums[0] + nums[1])
 
+
+def sub(nums):
+    print(nums[0] - nums[1])
+
+
+def mull(nums):
+    print(nums[0] * nums[1])
+
+
+def div(nums):
+    if nums[1] == 0:
+        print("ERROR - cant divide by zero!")
+        return
+    print(nums[0] / nums[1])
+
+
+def myPow(nums):
+    try:
+        print(nums[0] ** nums[1])
+    except:
+        print("Oops! Result too large...")
